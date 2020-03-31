7033a1451569a2d0df7ce40d3d6b2a8dd6a38ad4
nikhilrathor <nikhilrathor01@gmail.com>
2020-03-31 12:29:14 +0530

    Minor changes in app.js

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.get('/',function(req,res){
-    res.render('index',{
-      title:'Home'
-    });
-})
+var pages = require('./routes/pages.js')
+var adminPages = require('./routes/admin_pages.js')
 
-var port = 3000;
+app.use('/admin/pages',adminPages);
+app.use('/',pages);
+
+var port = 3200;
 app.listen(port,function(){
     console.log("Server started on port"+port);
 })
