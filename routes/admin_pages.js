0b023026eb70fe1e66570565c4623f3457ebe001
nikhilrathor <nikhilrathor01@gmail.com>
2020-03-31 22:33:52 +0530

    Database connected with Add Pages functionality

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -1,17 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var Page = require('../models/page')
+
 router.get('/', function (req, res) {
     res.send('Admin Area')
 })
 
 router.get('/add-page', function (req, res) {
-    
+
     var title = "";
     var slug = "";
     var content = "";
-    
-    res.render('admin/add_page',{
+
+    res.render('admin/add_page', {
         title: title,
         slug: slug,
         content: content
@@ -20,30 +22,53 @@ router.get('/add-page', function (req, res) {
 
 
 router.post('/add-page', function (req, res) {
-    
+
     req.checkBody('title', 'Title must have a value').notEmpty();
-    req.checkBody('content','Content must have a value').notEmpty();
-    
+    req.checkBody('content', 'Content must have a value').notEmpty();
+
     var title = req.body.title;
-    var slug = req.body.slug.replace(/\s+/g,'-').toLowerCase();
-    if(slug == "")
-        slug = title.replace(/\s+/g,'-').toLowerCase();
+    var slug = req.body.slug.replace(/\s+/g, '-').toLowerCase();
+    if (slug == "")
+        slug = title.replace(/\s+/g, '-').toLowerCase();
     var content = req.body.content;
 
     var errors = req.validationErrors();
-    
-    if(errors){
-        res.render('admin/add_page',{
+
+    if (errors) {
+        res.render('admin/add_page', {
             errors: errors,
             title: title,
             slug: slug,
             content: content
         });
     }
-    else{
-        console.log('success');
+    else {
+        Page.findOne({ slug: slug }, function (err, page) {
+            if (page) {
+                req.flash('danger', 'Page slug exists, choose another.');
+                res.render('admin/add_page', {
+                    title: title,
+                    slug: slug,
+                    content: content
+                });
+            }
+            else {
+                var page = new Page({
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    sorting: 0
+                });
+                page.save(function (err) {
+                    if (err)
+                        return console.log(err);
+                    req.flash('success', 'Page added!');
+                    res.redirect('/admin/pages');
+                });
+            }
+        });
     }
-    
+
 });
 
 
