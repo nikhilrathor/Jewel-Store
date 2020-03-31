f725957e3a13f5c13b58cca65f2d99edaacf75fc
nikhilrathor <nikhilrathor01@gmail.com>
2020-03-31 21:29:13 +0530

    Basic layout created for Add Page(routes, form UI)

--- routes/admin_pages.js
+++ routes/admin_pages.js
@@ -5,4 +5,17 @@ router.get('/', function (req, res) {
     res.send('Admin Area')
 })
 
+router.get('/add-page', function (req, res) {
+    
+    var title = "";
+    var slug = "";
+    var content = "";
+    
+    res.render('admin/add_page',{
+        title: title,
+        slug: slug,
+        content: content
+    });
+});
+
 module.exports = router;
