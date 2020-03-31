b3dcb1d69eefbd8a54383ae82fede85443ed2687
nikhilrathor <nikhilrathor01@gmail.com>
2020-03-31 13:07:23 +0530

    Added required middlewares(bodyParser,express-session,express-validator,express-messages,connect-flash ....)

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
 var express = require('express');
 var path = require('path');
 var mongoose = require('mongoose');
-var config = require('./config/database')
+var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
 
-mongoose.connect(config.database, {useUnifiedTopology: true, useNewUrlParser: true});
+mongoose.connect(config.database, { useUnifiedTopology: true, useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connected to mongodb')
+db.once('open', function () {
+  console.log('connected to mongodb');
 });
 
 var app = express();
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-var pages = require('./routes/pages.js')
-var adminPages = require('./routes/admin_pages.js')
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin/pages',adminPages);
-app.use('/',pages);
+app.use(bodyParser.json());
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+    var namespace = param.split('.')
+      , root = namespace.shift()
+      , formParam = root;
+
+    while (namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param: formParam,
+      msg: msg,
+      value: value
+    };
+  }
+}));
+
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/admin_pages.js');
+
+app.use('/admin/pages', adminPages);
+app.use('/', pages);
 
 var port = 3200;
-app.listen(port,function(){
-    console.log("Server started on port"+port);
+app.listen(port, function () {
+  console.log("Server started on port" + port);
 })
